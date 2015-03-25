5f7daba6f630d6989a618d816b5134af3f5fd52f
Joop vd Burgt <ketelbinkje@gmail.com>
2015-03-25 14:40:06 +0100

    feature(microposts) added rspec test illegal creation

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -5,4 +5,13 @@ describe Micropost do
   it 'There are no microposts' do
     expect(subject.all.count).to eql(0)
   end
+
+  it '#create a new micropost' do
+    expect(subject.create.id).to be_nil
+  end
+
+  #it '#drop a micropost' do
+  #  subject drop
+  #  expect(subject.all..count).to eql(0) and (subject.all.user) eql (User).user
+  #end
 end
